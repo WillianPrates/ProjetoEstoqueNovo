716ccb60c975e7a0d5ef8c4a492bd7ed0a36e840
WillianPrates <71357338+WillianPrates@users.noreply.github.com>
2021-08-30 16:24:12 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc= new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Entre com as informações dos produtos: ");
+        System.out.println("Nome: ");
+        product.name = sc.nextLine();
+        System.out.println("Price: ");
+        product.price = sc.nextDouble();
+        System.out.println("Quantidade em estoque: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.println("Entre com o nro de produtos a serem adicionados ao estoque: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.println("Entre com o nro de produtos a serem removidos ao estoque: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        sc.close();
+    }
+
+
+}
